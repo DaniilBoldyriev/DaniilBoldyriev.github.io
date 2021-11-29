0d23ac49e84e07db2b238347a77bfb16e58af3fc
Boldyrov Daniil <boldyrov@gmail.com>
2021-11-29 12:09:48 +0000

    1

--- js/main.js
+++ js/main.js
@@ -2,8 +2,9 @@ const marker = document.getElementById("marker");
 const promoVideo = document.getElementById("vid");
 const videoButton = document.querySelector(".play-video-button-wrapper");
 const videoEntity = document.querySelector("a-video");
-
+console.log('Worker')
 console.log(marker)
+
 function hideVideoButton() {
   videoButton.style.display = "none";
 }
